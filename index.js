4a0d2d67d11bfd888269879420c72c0b923fe3c9
Matthew Listor <mattlistor@gmail.com>
2019-04-16 02:46:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1)
 }
 
 function removeFirstKitten() {
-  kittens.slice(0, kittens.length - 1)
+  return kittens.slice(1)
 }
